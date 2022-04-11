5c3d4a571f4a2a9fc196145964d455f2ac96561b
Анна Гребенчук <anna.grebenchuk@gmail.com>
2022-04-11 19:17:14 +0300

    Финальная версия проекта

---
+++ yatube/posts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    # Главная страница
+    path('', views.index, name='index'),
+    # Страницы сообществ
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),
+]
